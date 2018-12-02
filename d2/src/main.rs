e9072b4b8d1c02be9b865a99266a969205deaa51
Fredrik Ferm <fredrik@fermitet.se>
2018-12-02 11:07:55 +0100

    D2-2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -1,41 +1,50 @@
 use std::fs;
-use std::collections::HashMap;
 
 fn main() {
     let file_contents = fs::read_to_string("input.txt").expect("Error in reading file");
 
-    let spl = file_contents.split("\n");
+    let words: Vec<&str> = file_contents.split("\n").collect();
 
-    let mut two_same = 0;
-    let mut three_same = 0;
+//    let mut two_same = 0;
+//    let mut three_same = 0;
 
-    for word in spl {
-        let chars = word.chars();
+    let num_words = words.len();
 
-        let mut chars_found = HashMap::new();
-
-        for c in chars {
-            let count = chars_found.entry(c).or_insert(0);
-            *count += 1;
-//            println!("c {}    count {}", c, count);
-        }
-
-        for (_, value) in &chars_found {
-            if *value == 2 {
-                two_same = two_same + 1;
-                break;
+    for i in 0..num_words {
+        for j in i..num_words {
+            if i == j {
+                continue;
             }
-        }
+            let word1 = words[i];
+            let word2 = words[j];
 
-        for (_, value) in &chars_found {
-            if *value == 3 {
-                three_same = three_same+ 1;
-                break;
+            let mut num_differences = 0;
+
+            for position in 0..word1.len() {
+
+                let c1 = word1.as_bytes()[position];
+                let c2 = word2.as_bytes()[position];
+                if c1 != c2 {
+                    num_differences = num_differences + 1;
+                }
             }
-        }
 
-        println!("{}", word);
+
+            if num_differences == 1 {
+                println!("Hittat en skillnad {} och {}", word1, word2);
+
+                for position in 0..word1.len() {
+
+                    let c1 = word1.as_bytes()[position];
+                    let c2 = word2.as_bytes()[position];
+                    if c1 == c2 {
+                        print!("{}", c1 as char);
+                    }
+                }
+                println!("");
+                panic!("Klar");
+            }
+            println!("i {}\tj {}\tword1 {}\tword2 {}\tnum_differences {}", i, j, word1, word2, num_differences);
+        }
     }
-
-    println!("two_same: {}      three_same {}       hash {}", two_same, three_same, two_same * three_same);
 }
