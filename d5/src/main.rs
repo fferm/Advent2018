33bd890ad6aad8fdd5bafd1be2682e794c2333ce
Fredrik Ferm <fredrik@fermitet.se>
2018-12-05 07:46:34 +0100

    D5-1

--- d5/src/main.rs
+++ d5/src/main.rs
@@ -1,24 +1,43 @@
 extern crate regex;
 
 use std::fs;
-use regex::Regex;
-use std::collections::HashMap;
 
 fn main() {
-    let inputs = read_inputs("input.txt");
-}
+    let mut working_string = read_inputs("input.txt");
 
-fn read_inputs(filename: &str) -> ??? {
-    let mut ret = ???
+    println!("{}  {}", working_string.len(), working_string);
 
-    let file_contents = fs::read_to_string(filename).expect("Error in reading file");
+    let mut did_change = true;
+    while did_change {
+        println!();
+        did_change = false;
 
-    let lines: Vec<&str> = file_contents.split("\n").collect();
+        for i in 0..working_string.len() - 1 {
+            let non_mutable = working_string.clone();
 
-    for line in lines {
-        ret.push(???);
+            let char1 = non_mutable.get(i..i + 1).unwrap();
+            let char2 = non_mutable.get(i + 1..i + 2).unwrap();
 
+//            println!("{} and {}", char1, char2);
+
+            if char1 != char2 && char1.to_uppercase() == char2.to_uppercase() {
+                let first_part = non_mutable.get(..i).unwrap();
+                let second_part = non_mutable.get(i + 2..).unwrap();
+
+                working_string = format!("{}{}", first_part, second_part);
+
+                println!("{}  {}", working_string.len(), working_string);
+                did_change = true;
+                break;
+            }
+        }
     }
 
-    return ret;
+
+}
+
+fn read_inputs(filename: &str) -> String {
+    let file_contents = fs::read_to_string(filename).expect("Error in reading file");
+
+    return file_contents;
 }
