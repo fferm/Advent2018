dc6d5c563440cd39fd1e4046277b19caf4c8a536
Fredrik Ferm <fredrik@fermitet.se>
2018-12-08 15:17:18 +0100

    D8-2

--- d8/src/main.rs
+++ d8/src/main.rs
@@ -1,11 +1,16 @@
 use std::fs;
+use std::cell::Cell;
 
 struct Node {
+    name: i32,
     child_quantity: i32,
     children: Vec<Node>,
     metadata_quantity: i32,
-    metadata: Vec<i32>
+    metadata: Vec<i32>,
+    value: Cell<i32>
 }
+
+static mut name: i32 = 0;
 
 fn main() {
     let small = false;
@@ -20,9 +25,9 @@ fn main() {
     let mut numbers = read_inputs(filename);
 
     let root = create_node(&mut numbers);
-    print_data(&root, 0);
+    let sum = analyze_sum_metadata(&root, 0);
 
-    let sum = analyze_sum_metadata(&root);
+    print_data(&root, 0);
 
     println!("Sum of metadata is: {}", sum);
 }
@@ -37,6 +42,7 @@ fn read_inputs(filename: &str) -> Vec<i32> {
 }
 
 fn create_node(numbers: &mut Vec<i32>) -> Node {
+
     let num_children = numbers.pop().unwrap();
 
     let num_metadata = numbers.pop().unwrap();
@@ -51,19 +57,48 @@ fn create_node(numbers: &mut Vec<i32>) -> Node {
         metadata.push(numbers.pop().unwrap());
     }
 
-    return Node{child_quantity: num_children, metadata_quantity: num_metadata, children: children, metadata: metadata};
+    unsafe {
+        name = name + 1;
+        return Node{name: name, child_quantity: num_children, metadata_quantity: num_metadata, children: children, metadata: metadata, value: Cell::new(0)};
+    }
 }
 
-fn analyze_sum_metadata(node: &Node) -> i32 {
+fn analyze_sum_metadata(node: &Node, num_tabs: i32) -> i32 {
     let mut ret = 0;
 
-    for md in &node.metadata {
+    for _i in 0..num_tabs {
+        print!("\t");
+    }
+    println!("Analyzing {}", node.name);
+
+    if node.child_quantity == 0 {
+        for md in &node.metadata {
+            ret += md;
+        }
+//        println!("Node {} has no children.  Value: {}", node.name, ret);
+    } else {
+        for md in &node.metadata {
+            if md <= &node.child_quantity {
+                ret += analyze_sum_metadata(&node.children.get((*md) as usize - 1).unwrap(), num_tabs + 1);
+            }
+        }
+//        println!("Node {} has children.  Value: {}", node.name, ret);
+    }
+
+    node.value.set(ret);
+
+    for _i in 0..num_tabs {
+        print!("\t");
+    }
+    println!("value of {} was {}", node.name, ret);
+
+/*    for md in &node.metadata {
         ret += md;
     }
 
     for child in &node.children {
         ret += analyze_sum_metadata(&child);
-    }
+    }*/
 
     return ret;
 }
@@ -73,7 +108,7 @@ fn print_data(node: &Node, num_tabs: i32) {
         print!("\t");
     }
 
-    println!("# children: {}, # metadata: {},    metadata: {:?}", node.child_quantity, node.metadata_quantity, node.metadata);
+    println!("name: {}   # children: {}, # metadata: {},  value: {}  metadata: {:?}", node.name, node.child_quantity, node.metadata_quantity, node.value.get(), node.metadata);
 
     for child in &node.children {
         print_data(&child, num_tabs + 1);
