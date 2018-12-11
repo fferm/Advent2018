02429e81d34665dbe48fa8de432f1c5c081ca897
Fredrik Ferm <fredrik@fermitet.se>
2018-12-11 11:43:39 +0100

    D11_2

--- d11/src/main.rs
+++ d11/src/main.rs
@@ -6,10 +6,17 @@ struct Coord {
     y: isize
 }
 
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Clone, Copy, Hash)]
+struct Grid {
+    top_left: Coord,
+    size: isize
+}
+
 fn main() {
     let grid_serial_number = 9435;
 
     let mut power_levels = HashMap::new();
+    let mut grids = HashMap::new();
 
     for x in 1..301 {
         for y in 1..301{
@@ -20,29 +27,27 @@ fn main() {
 
     let mut max_power = std::isize::MIN;
     let mut max_top_left = Coord{x: 0, y: 0};
+    let mut max_size = 0;
 
-    for y in 1..299 {
-        for x in 1..299 {
-            let c = Coord{x: x, y: y};
+    for size in 1..301 {
+        println!("Analyzing size {}", size);
+        for y in 1..302 - size {
+            for x in 1..302 - size {
+                let c = Coord{x: x, y: y};
 
-            let total_power = grid_power(c, &power_levels);
+                let total_power = grid_power(c, &power_levels, size, &mut grids);
 
-            if total_power > max_power {
-                max_power = total_power;
-                max_top_left = c.clone();
+                if total_power > max_power {
+                    max_power = total_power;
+                    max_top_left = c.clone();
+                    max_size = size.clone();
+                }
             }
         }
+
     }
 
-    for y in max_top_left.y..max_top_left.y+3 {
-        for x in max_top_left.x..max_top_left.x+3 {
-            let c = Coord{x: x, y: y};
-            print!("{:3} ", &power_levels.get(&c).unwrap());
-        }
-
-        println!();
-    }
-    println!("Max power: {} at {:?}", max_power, max_top_left);
+    println!("Max power: {} at {:?} with size: {}", max_power, max_top_left, max_size);
 
 
 }
@@ -60,12 +65,28 @@ fn power_level(c: Coord, grid_serial_number: isize) -> isize {
     return power_level;
 }
 
-fn grid_power(c: Coord, power_levels: &HashMap<Coord, isize>) -> isize {
+fn grid_power(c: Coord, power_levels: &HashMap<Coord, isize>, size: isize, grids: &mut HashMap<Grid, isize>) -> isize {
+    let smaller_grid = Grid{top_left: c.clone(), size: size - 1};
+
     let mut total_power = 0;
-    for dx in 0..3 {
-        for dy in 0..3 {
-            total_power = total_power + power_levels.get(&Coord{x: c.x + dx, y: c.y + dy}).unwrap()
+
+    if grids.contains_key(&smaller_grid) {
+        total_power = 0 + grids.get(&smaller_grid).unwrap();
+        for dx in 0..size {
+            total_power = total_power + power_levels.get(&Coord{x: c.x + dx, y: c.y + size - 1}).unwrap()
+        }
+        for dy in 0..size - 1 {
+            total_power = total_power + power_levels.get(&Coord{x: c.x + size - 1, y: c.y + dy}).unwrap()
+        }
+    } else {
+        for dx in 0..size {
+            for dy in 0..size {
+                total_power = total_power + power_levels.get(&Coord{x: c.x + dx, y: c.y + dy}).unwrap()
+            }
         }
     }
+
+    grids.insert(Grid{top_left:c.clone(), size: size}, total_power);
+
     return total_power;
 }
