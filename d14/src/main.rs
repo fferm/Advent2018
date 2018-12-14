e05a2febb322ad9d823b307bf19affca176361a7
Fredrik Ferm <fredrik.ferm@arbetsformedlingen.se>
2018-12-14 12:35:10 +0000

    D14-2

--- d14/src/main.rs
+++ d14/src/main.rs
@@ -1,31 +1,54 @@
 fn main() {
-    let small_input = false;
     let mut recepies = vec![3,7];
 
-    let num_recepies: usize;
-    if small_input {
-        num_recepies = 2018;
-    } else {
-        num_recepies = 846021;
-    }
+  //  let search_pattern: [isize; 5] = [5, 9, 4, 1, 4];
+    //let search_pattern: [isize; 6] = [ 9, 7, 8, 9, 6, 8];
+    let search_pattern: [isize; 6] = [8, 4, 6, 0, 2, 1];
 
+    let max_length = 1000000000;
     let mut current_idx1: usize =  0;
     let mut current_idx2: usize = 1;
 
     print_simulation(&recepies, current_idx1, current_idx2);
 
-    while recepies.len() < num_recepies + 10 {
+    let mut cont = true;
+    while cont {
         run_simulation(&mut recepies, &mut current_idx1, &mut current_idx2);
 
-        if num_recepies < 50 {
+        if recepies.len() < 20 {
             print_simulation(&recepies, current_idx1, current_idx2);
         } else if recepies.len() % 1000 == 0 {
-            println!("number of recepies: {},   target: {}", recepies.len(), num_recepies);
+            println!("number of recepies: {}", recepies.len());
+        }
+
+        if recepies.len() >= (search_pattern.len() + 1) {
+            let match_tuple = does_it_match(&recepies, &search_pattern);
+
+            if match_tuple.0 {
+                println!("Match at: {}", match_tuple.1);
+                cont = false;
+            }
+        }
+
+        if recepies.len() > max_length {
+            println!("För högt");
+            cont = false;
         }
     }
+}
 
+fn does_it_match(recepies: &Vec<isize>, search_pattern: &[isize]) -> (bool, usize) {
+    let part = &recepies[recepies.len() - search_pattern.len() .. recepies.len()];
+    if *part == *search_pattern {
+        return (true, recepies.len() - search_pattern.len());
+    }
 
-    print!("Last 10: {:?}", &recepies[num_recepies .. num_recepies + 10]);
+    let part_to_left = &recepies[recepies.len() - search_pattern.len() - 1 .. recepies.len() - 1];
+    if *part_to_left == *search_pattern {
+        return (true, recepies.len() - search_pattern.len() - 1);
+    }
+
+    return (false, 0);
 }
 
 fn run_simulation(recepies: &mut Vec<isize>, current_idx1: &mut usize, current_idx2: &mut usize) {
@@ -38,14 +61,16 @@ fn run_simulation(recepies: &mut Vec<isize>, current_idx1: &mut usize, current_i
 
     let sum = num1 + num2;
 
+/*    print!("num1: {}\tnum2: {}\tsum: {:2}\t\t", num1, num2, sum);*/
+
     if sum >= 10 {
         recepies.push(1);
     }
     let ones = sum % 10;
     recepies.push(ones);
 
-    *current_idx1 = (1 + *current_idx1 + num1 as usize) % recepies.len();
-    *current_idx2 = (1 + *current_idx2 + num2 as usize) % recepies.len();
+    *current_idx1 = (1 + *current_idx1 + (num1 as usize)) % recepies.len();
+    *current_idx2 = (1 + *current_idx2 + (num2 as usize)) % recepies.len();
 }
 
 fn print_simulation(recepies: &Vec<isize>, current_idx1: usize, current_idx2: usize) {
