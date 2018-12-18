d7bb110cd035aaee460430660c6a44c4d558b02e
Fredrik Ferm <fredrik.ferm@arbetsformedlingen.se>
2018-12-18 12:27:51 +0000

    More work on D17-1

--- d17/src/main.rs
+++ d17/src/main.rs
@@ -3,6 +3,7 @@ extern crate regex;
 use std::fs;
 use regex::Regex;
 use std::collections::HashSet;
+use std::cell::Cell;
 use std::fmt;
 
 fn main() {
@@ -17,15 +18,14 @@ fn main() {
     }
 
     let mut flowing_water = Vec::new();
-    let mut flow_heads = Vec::new();
 
-    let mut sim = read_inputs(filename, &mut flowing_water, &mut flow_heads);
+    let mut sim = read_inputs(filename, &mut flowing_water);
     if small_input {
         println!("{:?}", sim);
     }
 
     let mut step = 0;
-    while sim.run() {
+    while sim.run() && step < 403 {
         println!("Step: {}", step);
         if small_input {
             println!("{:?}", sim);
@@ -39,7 +39,7 @@ fn main() {
 
 }
 
-fn read_inputs<'a>(filename: &str, flowing_water: &'a mut Vec<Coord>, flow_heads: &'a mut Vec<Coord>) -> Sim<'a> {
+fn read_inputs<'a>(filename: &str, flowing_water: &'a mut Vec<Coord>) -> Sim<'a> {
     let file_contents = fs::read_to_string(filename).expect("Error in reading file");
 
     let lines: Vec<&str> = file_contents.split("\n").collect();
@@ -47,7 +47,13 @@ fn read_inputs<'a>(filename: &str, flowing_water: &'a mut Vec<Coord>, flow_heads
     let mut clay = HashSet::new();
     let settled_water = HashSet::new();
     let source = Coord { x: 500, y: 0 };
-    flow_heads.push(source.clone());
+
+    let down_from_source = source.mv(Direction::Down);
+
+    flowing_water.push(down_from_source.clone());
+
+    let flow_head = Cell::new(down_from_source.clone());
+
     let mut left = std::isize::MAX;
     let mut right = std::isize::MIN;
     let mut bottom = std::isize::MIN;
@@ -88,7 +94,7 @@ fn read_inputs<'a>(filename: &str, flowing_water: &'a mut Vec<Coord>, flow_heads
     left -= 1;
     right += 1;
 
-    return Sim { source, clay, settled_water, flowing_water, flow_heads, left, right, bottom };
+    return Sim { source, clay, settled_water, flowing_water, flow_head, left, right, bottom };
 }
 
 struct Sim<'a> {
@@ -96,7 +102,7 @@ struct Sim<'a> {
     clay: HashSet<Coord>,
     settled_water: HashSet<Coord>,
     flowing_water: &'a mut Vec<Coord>,
-    flow_heads: &'a mut Vec<Coord>,
+    flow_head: Cell<Coord>,
     left: isize,
     right: isize,
     bottom: isize,
@@ -104,103 +110,169 @@ struct Sim<'a> {
 
 impl<'a> Sim<'a> {
     fn run(&mut self) -> bool {
+        let did_flow = self.flow_if_possible();
+        if did_flow.is_some() {
+            return true;
+        }
 
-        // TODO: Skriv om
-
-        // 1 - bara en flow-head
-        // Om det går att flöda nedåt från denna, gör det (om inte nedåt är under kartan)
-        // Om inte nedåt, kolla om du är på fast mark (det finns lera under, eller vattenpelare som står på lera)
-        //      I så fall flöda vänster om det går
-        //      Annars flöda höger om det går
-        //
-        // Vid varje flöde ovan, nya positionen är ny flow head
-        //
-        // Om inget flöde ovan, sätt flow head till tidigare position och försök igen
-        //
-        // Om flow head kommer tillbaka till start, avbryt
-        //
-        // Detta är ju ingenting annat än en djupet-först-sökning.....
-
-
-
-
-        if self.flow_heads.len() > 0 {
-            let flow_head = self.flow_heads.pop().unwrap();
-
-            let down_from_flow_head = flow_head.mv(Direction::Down);
-            let left_from_flow_head = flow_head.mv(Direction::Left);
-            let right_from_flow_head = flow_head.mv(Direction::Right);
-
-            // Flow if possible
-            if self.can_flow_to(&down_from_flow_head) {
-                if down_from_flow_head.y <= self.bottom {
-                    self.flow_heads.push(down_from_flow_head);
-                    self.flowing_water.push(down_from_flow_head);
-                }
-            } else {
-                let mut can_go_left = false;
-                let mut can_go_right = false;
-
-                if self.can_flow_to(&left_from_flow_head) {
-                    self.flowing_water.push(left_from_flow_head);
-                    self.flow_heads.push(left_from_flow_head);
-                    can_go_left = true;
-                }
-                if self.can_flow_to(&right_from_flow_head) {
-                    self.flowing_water.push(right_from_flow_head);
-                    self.flow_heads.push(right_from_flow_head);
-                    can_go_right = true;
-                }
-
-                if !can_go_left && !can_go_right {
-                    println!("BLOCKED.  Flowhead: {:?}", flow_head);
-                }
-
-            }
+        // If we get here, it was impossible to flow
+        let settled_info = self.get_settled_info();
+        println!("settled_info: {:?}", settled_info);
+        if settled_info.is_some() {
+            self.settle(settled_info);
+            return true;
         } else {
-            let flow_size = self.flowing_water.len();
-            let latest_flow = self.flowing_water.get(flow_size - 1).unwrap().clone();
-            let second_latest = self.flowing_water.get(flow_size - 2).unwrap().clone();
+            // Det är inte settling som orsakar att vi inte kan flöda, det är istället att vi nått botten
+            loop {
+                let current = self.flow_head.get();
 
-            if latest_flow.y == second_latest.y {
-                // Handle blocked water
-                let mut left: Coord = latest_flow.clone();
-                let mut right: Coord = latest_flow.clone();
 
-                while !self.is_blocked_at(&left.mv(Direction::Left)) {
-                    left = left.mv(Direction::Left)
+                // TODO: Nu går flowing_water tillbaka efter att den nått botten
+                // Lägg in HashMap med status per koordinat i Sim istället för clay och settled
+                // Använd denna för flowing water också
+                // låt flowing-water vektorn och flow-head (kanske kan slås ihop) till att bara peka ut aktuell ström
+                // Gör display av flowing i vektorn och 'gammal' flowing olika, t ex 'F' för den ena
+                // Förmodligen behöver hantering av settled göras om så att man inte blir settlad förrän bägge sidor är låsta (annars blir det fel i undre koppen som har olika höjd på sidorna
+
+                if self.is_on_solid_ground(&current) && (self.can_flow_to(&current.mv(Direction::Left)) || self.can_flow_to(&current.mv(Direction::Right))) {
+                    break;
                 }
-
-                while !self.is_blocked_at(&right.mv(Direction::Right)) {
-                    right = right.mv(Direction::Right)
-                }
-
-                for x in left.x..right.x + 1 {
-                    let coord = Coord { x: x, y: latest_flow.y };
-                    self.settled_water.insert(coord);
-                    self.flowing_water.pop();
-                }
-
-                let new_flow_head = self.flowing_water.get(self.flowing_water.len() - 1).unwrap();
-                self.flow_heads.push(new_flow_head.clone());
-            } else {
-                return false;
+                self.flow_head.set(self.flowing_water.pop().unwrap().clone());
             }
         }
 
-        return true;
+        return false;
+    }
+
+    fn flow_if_possible(&mut self) -> Option<Direction> {
+        if self.flow_in_direction(Direction::Down) {
+            return Some(Direction::Down);
+        }
+        if self.flow_in_direction(Direction::Left) {
+            return Some(Direction::Left);
+        }
+        if self.flow_in_direction(Direction::Right) {
+            return Some(Direction::Right);
+        }
+
+        return None;
+    }
+
+    fn flow_in_direction(&mut self, dir: Direction) -> bool {
+        let c = self.flow_head.get().mv(dir);
+        if self.can_flow_to(&c) {
+            self.flow_head.set(c.clone());
+            self.flowing_water.push(c.clone());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    fn get_settled_info(&self) -> Option<(Coord, Coord, bool)> {  // Option<(Left, Right, other_side_reaches_sand)>
+        if !self.is_on_solid_ground(&self.flow_head.get()) {
+            return None;
+        }
+
+        let mut left: Coord = self.flow_head.get().clone();
+        let mut right: Coord = self.flow_head.get().clone();
+
+        let mut other_side_reaches_sand = false;
+
+        loop {
+            let further = left.mv(Direction::Left);
+            let contents_of = self.get_contents(&further);
+
+            match contents_of {
+                Contents::Sand => {
+                    other_side_reaches_sand = true;
+                    break;
+                },
+                Contents::Flowing => left = further,
+                _ => break
+            }
+        }
+
+        loop {
+            let further = right.mv(Direction::Right);
+            let contents_of = self.get_contents(&further);
+
+            match contents_of {
+                Contents::Sand => {
+                    other_side_reaches_sand = true;
+                    break;
+                },
+                Contents::Flowing => right = further,
+                _ => break
+            }
+        }
+
+        return Some((left, right, other_side_reaches_sand));
+    }
+
+    fn settle(&mut self, settled_info_option: Option<(Coord, Coord, bool)>) {
+        if settled_info_option.is_none() {
+            return;
+        }
+        let settled_info = settled_info_option.unwrap();
+        let mut start = self.flow_head.get();
+
+        let mut current = start.clone();
+        while current.is_inside_including_edge(&settled_info.0, &settled_info.1) {
+            self.settled_water.insert(current);
+            current = self.flowing_water.pop().unwrap();
+        }
+
+        self.flowing_water.push(current.clone());
+
+        let other_side_reaches_sand = settled_info.2;
+
+        if !other_side_reaches_sand {
+            self.flow_head.set(current.clone());
+        } else {
+            self.flow_head.set(current.mv(Direction::Down));
+        }
     }
 
     fn can_flow_to(&self, target: &Coord) -> bool {
-        return !self.is_blocked_at(target) && !self.flowing_water.contains(target);
+        if !self.is_inside(target) {
+            return false;
+        }
+        return !self.clay.contains(target) && !self.settled_water.contains(target) && !self.flowing_water.contains(target);
     }
 
     fn is_blocked_at(&self, target: &Coord) -> bool {
         return self.clay.contains(target) || self.settled_water.contains(target);
     }
 
+    fn is_on_solid_ground(&self, target: &Coord) -> bool {
+        let below = target.mv(Direction::Down);
+
+        return self.clay.contains(&below) || self.settled_water.contains(&below);
+    }
+
+    fn is_inside(&self, target: &Coord) -> bool {
+        return target.x >= self.left && target.x <= self.right && target.y <= self.bottom;
+    }
+
     fn num_water(&self) -> usize {
         return self.flowing_water.len() + self.settled_water.len();
+    }
+
+    fn get_contents(&self, target: &Coord) -> Contents {
+        if self.settled_water.contains(target) {
+            return Contents::Settled;
+        }
+
+        if self.clay.contains(target) {
+            return Contents::Clay;
+        }
+
+        if self.flowing_water.contains(target) {
+            return Contents::Flowing;
+        }
+
+        return Contents::Sand;
     }
 }
 
@@ -214,6 +286,8 @@ impl<'a> fmt::Debug for Sim<'a> {
                 let mut ch = ".";
                 if self.clay.contains(&coord) {
                     ch = "#";
+                } if self.flow_head.get() == coord {
+                    ch = "0";
                 } else if self.flowing_water.contains(&coord) {
                     ch = "|";
                 } else if self.settled_water.contains(&coord) {
@@ -226,15 +300,19 @@ impl<'a> fmt::Debug for Sim<'a> {
             }
             ret.push_str(&"\n");
         }
-        ret.push_str(&"Flow heads: ");
-        for flow_head in self.flow_heads.clone() {
-            ret.push_str(&format!("{:?}", flow_head));
-            ret.push_str(&", ");
-        }
+
         ret.push_str(&"\n");
 
         return write!(f, "{}", ret);
     }
+}
+
+#[derive(Eq, PartialEq, Hash, Copy, Clone)]
+enum Contents {
+    Clay,
+    Flowing,
+    Settled,
+    Sand
 }
 
 #[derive(Eq, PartialEq, Hash, Copy, Clone)]
@@ -251,6 +329,22 @@ impl Coord {
             Direction::Up => Coord { x: self.x, y: self.y - 1 },
             Direction::Down => Coord { x: self.x, y: self.y + 1 }
         }
+    }
+
+    fn is_inside_including_edge(&self, top_left: &Coord, bottom_right: &Coord) -> bool {
+        if self.x < top_left.x {
+            return false;
+        }
+        if self.y < top_left.y {
+            return false;
+        }
+        if self.x > bottom_right.x {
+            return false;
+        }
+        if self.y > bottom_right.y {
+            return false;
+        }
+        return true;
     }
 }
 
