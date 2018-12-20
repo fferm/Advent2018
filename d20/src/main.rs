1fc72358b157e26d85c79b4d1ffdcefa66654d35
Fredrik Ferm <fredrik.ferm@arbetsformedlingen.se>
2018-12-20 12:12:57 +0000

    Working on input handling in D20

--- d20/src/main.rs
+++ d20/src/main.rs
@@ -17,83 +17,90 @@ fn main() {
     }
 
     let mut contents = HashMap::new();
-    let mut world = World::new(&mut contents);
-
-//    let mut world = read_inputs(filename, &mut contents);
-
-    world.insert(&Coord::new(5, 3), Contents::Door);
-    world.insert(&Coord::new(0, 0), Contents::Room);
-    world.insert(&Coord::new(1, 1), Contents::Wall);
-
-    let mut steps = HashSet::new();
+    let mut world = read_inputs(filename, &mut contents);
     println!("{:?}", world);
-
-    let input = read_inputs(filename, steps);
-    println!("{:?}", input)
 }
 
-fn read_inputs<'a>(filename: &str, steps: HashSet<Step<'a>>) -> Input<'a> {
+fn read_inputs<'a>(filename: &str, contents: &'a mut HashMap<Coord, Contents>) -> World<'a> {
     let file_contents = fs::read_to_string(filename).expect("Error in reading file");
+    let mut world = World::new(contents);
 
-    for idx in 0..file_contents.len() {
-        let c =file_contents.get(idx..idx+1).unwrap();
-        println!("{}", c);
-    }
-    
-    return Input{steps, }
+    add_contents(&mut world, &file_contents[1 .. file_contents.len() - 1], &Coord::new(0, 0));
 
-
-
-
-/*    let mut acres = HashMap::new();
-    let mut x_size = 0;
-    let mut y_size = 0;
-    let mut y = 0;
-    for line in lines {
-        for x in 0..line.len() {
-            let coord = Coord{x, y};
-
-            let c = line.get(x..x+1).unwrap();
-
-            if c == "." {
-                acres.insert(coord, State::Open);
-            } else if c == "#" {
-                acres.insert(coord, State::Lumberyard);
-            } else if c == "|" {
-                acres.insert(coord, State::Trees);
-            }
-
-            if x > x_size && acres.contains_key(&coord) {
-                x_size = x;
-            }
-
-        }
-        if y > y_size {
-            y_size = y
-        }
-
-        y += 1;
-    }
-
-    return World{acres, x_size, y_size};*/
+    return world;
 }
 
-#[derive(Debug)]
-struct Input<'a> {
-    steps: HashSet<Step<'a>>,
-    head: &'a Step<'a>
+fn add_contents<'a>(world: &'a mut World, input: &str, starting_point: &Coord) -> Coord {
+    println!("Input: {}   start: {:?}", input, starting_point);
+
+    let paren_idx_opt = input.find("(");
+    let split_idx_opt = input.find("|");
+
+    if paren_idx_opt.is_none() && split_idx_opt.is_none() {
+        return add_contents_in_line(world, input, starting_point);
+    } else if paren_idx_opt.is_some() && split_idx_opt.is_none() {
+        return add_contents_from_paren(world, input, starting_point, paren_idx_opt.unwrap());
+    } else if paren_idx_opt.is_none() && split_idx_opt.is_some() {
+        return add_contents_from_split(world, input, starting_point, split_idx_opt.unwrap());
+    } else {
+        let paren_idx = paren_idx_opt.unwrap();
+        let split_idx = split_idx_opt.unwrap();
+
+        if paren_idx < split_idx {
+            return add_contents_from_paren(world, input, starting_point, paren_idx_opt.unwrap());
+        } else {
+            return add_contents_from_split(world, input, starting_point, split_idx_opt.unwrap());
+        }
+    }
+
 }
 
-#[derive(Eq, PartialEq, Hash, Copy, Clone)]
-struct Step<'a> {
-    direction: Direction,
+fn add_contents_from_paren<'a>(world: &'a mut World, input: &str, starting_point: &Coord, paren_idx: usize) -> Coord {   // returns end point
+    let before_str = &input[0..paren_idx];
+    let after_str = &input[paren_idx + 1 .. input.len() - 1];
 
-    // TODO, flera next behöver vara möjliga
-    next: &'a Step<'a>
+    let midpoint = add_contents(world, before_str, starting_point);
+    let endpoint = add_contents(world, after_str, &midpoint);
+
+    return endpoint;
+}
+
+fn add_contents_from_split<'a>(world: &'a mut World, input: &str, starting_point: &Coord, split_idx: usize) -> Coord {   // returns end point
+    let before_str = &input[0..split_idx];
+    let after_str = &input[split_idx + 1 .. input.len()];
+
+    let midpoint = add_contents(world, before_str, starting_point);
+    let endpoint = add_contents(world, after_str, starting_point);
+
+    return starting_point.clone();
+}
+
+fn add_contents_in_line<'a>(world: &'a mut World, input: &str, starting_point: &Coord) -> Coord {   // returns end point
+    let mut current_point = starting_point.clone();
+
+    let mut idx = 0;
+    while idx < input.len() {
+        let c =input.get(idx..idx+1).unwrap();
+
+        if "NESW".contains(c) {
+            let direction = Direction::from_input(c);
+
+            let door_coord = current_point.mv(direction);
+            world.insert(&door_coord, Contents::Door);
+
+            let room_coord = door_coord.mv(direction);
+            world.insert(&room_coord, Contents::Room);
+
+            current_point = room_coord.clone();
+            idx += 1;
+        }
+    }
+    return current_point;
 }
 
 struct World<'a> {
     contents: &'a mut HashMap<Coord, Contents>,
+    starting_point: Coord,
     x_min: isize,
     x_max: isize,
     y_min: isize,
@@ -102,11 +109,16 @@ struct World<'a> {
 
 impl<'a> World<'a> {
     fn new(contents: &'a mut HashMap<Coord, Contents>) -> World<'a> {
-        return World{contents, x_min: 0, x_max: 0, y_min: 0, y_max: 0};
+        let starting_point = Coord::new(0, 0);
+        let mut world = World{contents, starting_point, x_min: 0, x_max: 0, y_min: 0, y_max: 0};
+
+        world.insert(&starting_point, Contents::Start);
+
+        return world;
     }
 
     fn get_contents(&self, target: &Coord) -> Contents {
-        return *self.contents.get(target).unwrap_or(&Contents::Unknown);
+        return *self.contents.get(target).unwrap_or(&Contents::Wall);
     }
 
     fn insert(&mut self, coord: &Coord, state: Contents) {
@@ -131,8 +143,8 @@ impl<'a> fmt::Debug for World<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut ret = "".to_owned();
 
-        for y in self.y_min..self.y_max + 1 {
-            for x in self.x_min..self.x_max + 1 {
+        for y in self.y_min - 1..self.y_max + 2 {
+            for x in self.x_min - 1..self.x_max + 2 {
                 let coord = Coord::new(x, y);
 
                 let contents = self.get_contents(&coord);
@@ -155,7 +167,7 @@ enum Contents {
     Door,
     Room,
     Wall,
-    Unknown
+    Start
 }
 
 impl fmt::Debug for Contents {
@@ -163,10 +175,10 @@ impl fmt::Debug for Contents {
         let mut letter = " ";
 
         match self {
-            Contents::Door => letter = "|",
+            Contents::Door => letter = "+",
             Contents::Room => letter = ".",
             Contents::Wall => letter = "#",
-            Contents::Unknown => letter = " "
+            Contents::Start => letter = "X"
         }
 
         return write!(f, "{}", letter);
@@ -187,8 +199,8 @@ impl Coord {
 
     fn mv(&self, dir: Direction) -> Coord {
         match dir {
-            Direction::East => Coord { x: self.x - 1, y: self.y },
-            Direction::West => Coord { x: self.x + 1, y: self.y },
+            Direction::West => Coord { x: self.x - 1, y: self.y },
+            Direction::East => Coord { x: self.x + 1, y: self.y },
             Direction::North => Coord { x: self.x, y: self.y - 1 },
             Direction::South => Coord { x: self.x, y: self.y + 1 }
         }
@@ -208,6 +220,18 @@ enum Direction {
     East,
     West,
     South,
+}
+
+impl Direction {
+    fn from_input(input: &str) -> Direction {
+        match input {
+            "N" => return Direction::North,
+            "E" => return Direction::East,
+            "W" => return Direction::West,
+            "S" => return Direction::South,
+            _ => panic!("Unknown direction input: {}", input)
+        }
+    }
 }
 
 impl fmt::Debug for Direction {
