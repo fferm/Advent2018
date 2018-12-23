716201f767f26a7ba282e622ade66c8deb3f4024
Fredrik Ferm <fredrik@fermitet.se>
2018-12-23 16:40:56 +0100

    D23-2

--- d23/src/main.rs
+++ d23/src/main.rs
@@ -4,10 +4,9 @@ use std::fs;
 use std::fmt;
 use regex::Regex;
 use std::collections::HashSet;
-use std::collections::HashMap;
 
 fn main() {
-    let small_input = true;
+    let small_input = false;
     let filename;
 
     if small_input {
@@ -18,23 +17,72 @@ fn main() {
 
     let nanobots = read_inputs(filename);
 
-    let mut num_bots_in_range = HashMap::new();
-    for bot in nanobots.clone() {
-        println!("Analyzing bot at: {:?} with range: {}", bot.pos, bot.range);
-
-        for point in bot.pos.points_within_distance(bot.range) {
-            let mut entry = num_bots_in_range.entry(point).or_insert(0);
-            *entry += 1;
+    let mut vertexes = HashSet::new();
+    for nanobot in nanobots.clone() {
+        for vertex in nanobot.vertexes_from() {
+            vertexes.insert(vertex);
         }
     }
 
-    println!("{:?}", num_bots_in_range);
+//    let mut vertexes_in_range: HashMap<Coord, isize> = HashMap::new();
+    let mut max_num = 0;
+    let mut max_vertex = Coord::new(0, 0, 0);
+    for vertex in vertexes {
+        let num_nanobots_in_range = num_nanobots_in_range_from(&vertex, &nanobots);
 
+        if num_nanobots_in_range > max_num {
+            max_num = num_nanobots_in_range;
+            max_vertex = vertex.clone();
+            println!("Upping num to {}  from point {:?}", max_num, max_vertex);
+        } else if num_nanobots_in_range == max_num {
+            println!("Also {:?} has {}", vertex, num_nanobots_in_range);
+        }
 
+    }
+
+    println!("Vertex: {:?}   num in range: {}     manhattan_distance: {}", max_vertex, max_num, max_vertex.manhattan_distance_from(Coord::new(0, 0, 0)));
+
+    let mut point = max_vertex.clone();
+    let mut range = max_num.clone();
+    let mut distance = 0;
+
+    let mut cont = true;
+    while cont {
+        cont = false;
+        distance += 1;
+
+        let points_at_distance = point.points_at_distance(distance);
+        for other_point in points_at_distance {
+            let other_range = num_nanobots_in_range_from(&other_point, &nanobots);
+
+            if other_range >= range {
+                println!("Vertex: {:?}   num in range: {}     manhattan_distance: {}", other_point, other_range, other_point.manhattan_distance_from(Coord::new(0, 0, 0)));
+                cont = true;
+            }
+
+            if other_range > range {
+                range = other_range;
+                point = other_point;
+                distance = 0;
+            }
+        }
+
+    }
 }
 
-fn read_inputs<'a>(filename: &str) -> HashSet<Nanobot> {
-    let mut ret = HashSet::new();
+fn num_nanobots_in_range_from(coord: &Coord, nanobots: &Vec<Nanobot>) -> isize {
+    let mut num_nanobots_in_range = 0;
+    for nanobot in nanobots.iter() {
+        if coord.manhattan_distance_from(nanobot.pos) <= nanobot.range {
+            num_nanobots_in_range += 1;
+        }
+    }
+
+    return num_nanobots_in_range;
+}
+
+fn read_inputs<'a>(filename: &str) -> Vec<Nanobot> {
+    let mut ret = Vec::new();
 
     let file_contents = fs::read_to_string(filename).expect("Error in reading file");
     let lines: Vec<&str> = file_contents.split("\n").collect();
@@ -45,25 +93,39 @@ fn read_inputs<'a>(filename: &str) -> HashSet<Nanobot> {
 
         let pos = Coord::new(cap[1].parse().unwrap(), cap[2].parse().unwrap(), cap[3].parse().unwrap());
         let nanobot = Nanobot{pos, range: cap[4].parse().unwrap()};
-        ret.insert(nanobot);
+        ret.push(nanobot);
     }
 
     return ret;
 }
 
-#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone, Ord, PartialOrd)]
+#[derive(Eq, PartialEq, Hash, Copy, Clone, Ord, PartialOrd)]
 struct Nanobot {
     pos: Coord,
     range: isize
 }
 
 impl Nanobot {
-    fn other_is_in_range_of(&self, other: &Nanobot) -> bool {
-        let distance = self.pos.manhattan_distance_from(other.pos);
+    fn vertexes_from(&self) -> HashSet<Coord> {
+        let mut ret = HashSet::new();
 
-        return distance <= self.range;
+        ret.insert(Coord::new(self.pos.x + self.range, self.pos.y, self.pos.z));
+        ret.insert(Coord::new(self.pos.x - self.range, self.pos.y, self.pos.z));
+        ret.insert(Coord::new(self.pos.x, self.pos.y + self.range, self.pos.z));
+        ret.insert(Coord::new(self.pos.x, self.pos.y - self.range, self.pos.z));
+        ret.insert(Coord::new(self.pos.x, self.pos.y, self.pos.z + self.range));
+        ret.insert(Coord::new(self.pos.x, self.pos.y, self.pos.z - self.range));
+
+        return ret;
     }
 }
+
+impl fmt::Debug for Nanobot {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        return writeln!(f, "pos={:?}   range={}", self.pos, self.range);
+    }
+}
+
 
 #[derive(Eq, PartialEq, Hash, Copy, Clone, Ord, PartialOrd)]
 struct Coord {
@@ -103,44 +165,31 @@ impl Coord {
         return x_dist + y_dist + z_dist;
     }
 
-    fn points_within_distance(&self, distance: isize) -> Vec<Coord> {
-        let mut ret = Vec::new();
-
-        ret.push(self.clone());
-
-        for current_distance in 0 .. distance + 1 {
-            let points_at_distance = self.points_at_distance(current_distance);
-
-            for p in points_at_distance {
-                ret.push(p);
-            }
-        }
-
-        return ret;
-
-    }
-
     fn points_at_distance(&self, dist: isize) -> HashSet<Coord> {
         let mut ret = HashSet::new();
 
-        for dx in -dist..dist+1 {
+        for dx in 0 .. dist+1 {
             let y_dist = dist - dx.abs();
 
-            for dy in -y_dist .. y_dist + 1 {
-                let dz = y_dist - dy.abs();
+            for dy in 0 .. y_dist + 1 {
+                let z_dist = y_dist - dy.abs();
 
-                ret.insert(Coord::new(self.x + dx, self.y + dy, self.z + dz));
-                ret.insert(Coord::new(self.x + dx, self.y + dy, self.z - dz));
-                ret.insert(Coord::new(self.x + dx, self.y - dy, self.z + dz));
-                ret.insert(Coord::new(self.x + dx, self.y - dy, self.z - dz));
-                ret.insert(Coord::new(self.x - dx, self.y + dy, self.z + dz));
-                ret.insert(Coord::new(self.x - dx, self.y + dy, self.z - dz));
-                ret.insert(Coord::new(self.x - dx, self.y - dy, self.z + dz));
-                ret.insert(Coord::new(self.x - dx, self.y - dy, self.z - dz));
+//                for dz in 0 .. z_dist + 1 {
+                for dz in z_dist .. z_dist + 1 {
+                    ret.insert(Coord::new(self.x + dx, self.y + dy, self.z + dz));
+                    ret.insert(Coord::new(self.x + dx, self.y + dy, self.z - dz));
+                    ret.insert(Coord::new(self.x + dx, self.y - dy, self.z + dz));
+                    ret.insert(Coord::new(self.x + dx, self.y - dy, self.z - dz));
+                    ret.insert(Coord::new(self.x - dx, self.y + dy, self.z + dz));
+                    ret.insert(Coord::new(self.x - dx, self.y + dy, self.z - dz));
+                    ret.insert(Coord::new(self.x - dx, self.y - dy, self.z + dz));
+                    ret.insert(Coord::new(self.x - dx, self.y - dy, self.z - dz));
+                }
             }
         }
         return ret;
     }
+
 
 
 }
